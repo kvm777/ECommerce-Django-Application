642063899172b03c5601effac8d0c81fbc897b5a
mahesh <koradamahesh2000@example.com>
2024-03-21 11:34:11 +0530

    adding vercel file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xxta((n3gwm!n^+p@7a)!chs#_27#h+1tu^4xo9a5wweg%)oyv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ".vercel.app"]
 
 
 # Application definition
